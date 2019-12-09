021a63cd21eb2c8265de15ad59586125a9b38d8e
minhnguyenjirnexu <minh.nguyen@jirnexu.com>
2019-12-09 17:44:22 +0700

    Initial Upload Image

---
+++ server.js
@@ -0,0 +1,54 @@
+// call all the required packages
+const express = require('express')
+const bodyParser= require('body-parser')
+const multer = require('multer');
+var cors = require('cors');
+//CREATE EXPRESS APP
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().getTime() + '_' + file.originalname.replace(/\s/gm, '_'))
+    }
+})
+   
+var upload = multer({ storage: storage })
+
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+    const file = req.file
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+      res.send(file)
+    
+})
+
+//Uploading multiple files
+app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
+    const files = req.files
+    if (!files) {
+      const error = new Error('Please choose files')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+   
+      res.send(files)
+    
+})
+
+//ROUTES WILL GO HERE
+app.get('/', function(req, res) {
+    res.json({ message: 'WELCOME' });   
+});
+ 
+app.listen(3001, () => console.log('Server started on port 3000'));
